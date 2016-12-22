113153ddec7fcbd7976cb18827f812071c0cef09
Nantha Harish JR <harish-rajendran@users.noreply.github.com>
2016-12-22 13:45:38 +0530

    Update route.py

--- route.py
+++ route.py
@@ -11,9 +11,9 @@ application.config['CORS_HEADERS'] = 'Content-Type'
 @cross_origin()
 def signup():
     url = 'http://localhost:5002/vendor/signup/'
-    d = {'vendorid' :'1' ,'storename' : 'MAD','storeid' : '200','city' : 'Chennai','branch' : 'Race Club','state' : 'TamilNadu'}
+    value = {'vendorid' :'1' ,'storename' : 'MAD','storeid' : '200','city' : 'Chennai','branch' : 'Race Club','state' : 'TamilNadu'}
     head = {'Content-Type' : 'application/json'}
-    r=requests.post(url, data=json.dumps(d), headers=head) 
+    r=requests.post(url, data=json.dumps(value), headers=head) 
     return r.text
     
 
@@ -21,45 +21,45 @@ def signup():
 @cross_origin()
 def info():
     url = 'http://localhost:5002/vendor/details/'
-    d = {'vendorid' : 1 ,'storeid' : "all" }
+    value = {'vendorid' : 1 ,'storeid' : "all" }
     head = {'Content-Type' : 'application/json'}
-    r=requests.get(url, data=json.dumps(d), headers=head) 
+    r=requests.get(url, data=json.dumps(value), headers=head) 
     return r.text
 
 @application.route('/test/update')
 @cross_origin()
 def update():
     url = 'http://localhost:5002/vendor/details/update/'
-    d = {'vendorid' : 1 ,'storename' : 'grey','storeid' : 5200,'city' : 'Chennai','branch' : 'Poes','state' : 'Tamilnadu'}
+    value = {'vendorid' : 1 ,'storename' : 'grey','storeid' : 5200,'city' : 'Chennai','branch' : 'Poes','state' : 'Tamilnadu'}
     head = {'Content-Type' : 'application/json'}
-    r=requests.get(url, data=json.dumps(d), headers=head) 
+    r=requests.get(url, data=json.dumps(value), headers=head) 
     return r.text
 
 @application.route('/test/delete')
 @cross_origin()
 def delete():
     url = 'http://localhost:5002/vendor/details/delete/'
-    d = {'vendorid' : 1 ,'storeid' : 2200 }
+    value = {'vendorid' : 1 ,'storeid' : 2200 }
     head = {'Content-Type' : 'application/json'}
-    r=requests.get(url, data=json.dumps(d), headers=head) 
+    r=requests.get(url, data=json.dumps(value), headers=head) 
     return r.text
 
 @application.route('/test/downloadcsv')
 @cross_origin()
 def downloadcsv():
     url = 'http://localhost:5002/vendor/downloadcsv/'
-    d = {'vendorid' : 1 }
+    value = {'vendorid' : 1 }
     head = {'Content-Type' : 'application/json'}
-    r=requests.get(url, data=json.dumps(d), headers=head) 
+    r=requests.get(url, data=json.dumps(value), headers=head) 
     return r.text
 
 @application.route('/test/downloadpdf')
 @cross_origin()
 def downloadpdf():
     url = 'http://localhost:5002/vendor/downloadpdf/'
-    d = {'vendorid' : 1  }
+    value = {'vendorid' : 1  }
     head = {'Content-Type' : 'application/json'}
-    r=requests.get(url, data=json.dumps(d), headers=head) 
+    r=requests.get(url, data=json.dumps(value), headers=head) 
     return r.text
 
 
